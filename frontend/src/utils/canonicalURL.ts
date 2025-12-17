bc53d5396e018076a2cdb1970529b17e05723aeb
Amogh Vijay <amoghvj@gmail.com>
2025-12-17 10:28:12 +0530

    Stitch 1.6.0
    Modified URL normalization to support Devfolio.

--- frontend/src/utils/canonicalURL.ts
+++ frontend/src/utils/canonicalURL.ts
@@ -1,8 +1,9 @@
 function canonicalURL(url: string): string {
     let raw = new URL(url);
 
+    if (raw.hostname.endsWith('devfolio.co')) return `https://${raw.hostname}/`;
+
     return `https://${raw.hostname}${raw.pathname}`;
 }
 
 export { canonicalURL };
-
